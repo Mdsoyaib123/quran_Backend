5ec5e72be6cf155d87c7bcbd5760132c7956038a
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2026-04-17 20:59:17 +0600

    added the latest update

--- src/module/route/quran.routes.ts
+++ src/module/route/quran.routes.ts
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get("/surahs", QuranController.getAllSurahs);
 router.get("/surah/:id", QuranController.getSingleSurah);
+
 router.get("/search", QuranController.searchAyahs);
 
 export const QuranRoutes = router;
